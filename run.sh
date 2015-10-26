b03fb68cabb78ebdf8c13418e11cf37cf719fe3a
Alexei Ustyuzhaninov <alust@alust.homeunix.com>
2015-10-26 18:12:16 +0500

    Debugging

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-while ps ax|grep cloud | grep -v grep
-do
-  sleep 1
-done
-chef-client --local-mode --runlist 'recipe[test_task::base]'
+set
+chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
+echo 10 seconds
+sleep 10
+chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
+echo 60 seconds
+sleep 60
+chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
