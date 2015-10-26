4e7bec7e8482e7ab7ab073d2b7e2745cb51a05c0
Alexei Ustyuzhaninov <alust@alust.homeunix.com>
2015-10-26 18:22:23 +0500

    Debugging

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 set
-chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
+chef-client -l debug --local-mode --runlist 'recipe[test_task::base]'
 echo 10 seconds
 sleep 10
-chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
+chef-client -l debug  --local-mode --runlist 'recipe[test_task::base]'
 echo 60 seconds
 sleep 60
-chef-client -l debug -L /tmp/chef.log -V --local-mode --runlist 'recipe[test_task::base]'
+chef-client -l debug  --local-mode --runlist 'recipe[test_task::base]'
